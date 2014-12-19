ffe0d57d71ed0636a0801306fe2da26b96cd3be5
Mark Borcherding <markborcherding@gmail.com>
2014-12-18 21:11:28 -0600

    add checksum

---
+++ lib/kata_bank_ocr/checksum.rb
@@ -0,0 +1,22 @@
+module KataBankOcr
+  # The class to  an input file.
+  module Checksum
+    # Check the checksum of the account number
+    #
+    # @param account_number [String] The account number to
+    #                                test
+    #
+    # @return [Boolean] `true` if the checksum is valid
+    def self.valid?(account_number)
+      sum = account_number
+            .chars
+            .map(&:to_i)
+            .reverse
+            .each_with_index
+            .reduce(0) do |a, e|
+              a + (e[0] * (e[1] + 1))
+            end
+      (sum % 11) == 0
+    end
+  end
+end
---
+++ spec/features/user_story_2_spec.rb
@@ -0,0 +1,21 @@
+require_relative '../../lib/kata_bank_ocr/checksum'
+
+RSpec.describe 'User Story 1' do
+  context 'checking an account number checksum' do
+    %w( 711111111
+        123456789
+        490867715).each do |valid|
+      it "affirms #{valid} is a valid account number" do
+        expect(KataBankOcr::Checksum.valid?(valid)).to eq(true)
+      end
+    end
+
+    %w( 888888888
+        490067715
+        012345678).each do |invalid|
+      it "invalidates #{invalid} as a valid account number" do
+        expect(KataBankOcr::Checksum.valid?(invalid)).to eq(false)
+      end
+    end
+  end
+end
